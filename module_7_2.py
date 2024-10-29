977b23459ee518cf71e0766767e4a20be59d6382
Константин <tractorx@yandex.ru>
2024-10-29 21:57:43 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,27 @@
+def custom_write(file_name, strings):
+
+    file = open(file_name, 'a', encoding= 'utf-8')
+
+    strings_positions = {}
+
+    for i in range(len(strings)):
+        a = i + 1
+        b = file.tell()
+        file.write(strings[i] + '\n')
+        strings_positions[(a, b)] = strings[i]
+
+    file.close()
+
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
